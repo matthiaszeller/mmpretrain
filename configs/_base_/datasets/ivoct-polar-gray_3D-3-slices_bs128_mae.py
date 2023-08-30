3d8a898cabeb6d8f1af17e5b1e74970219f13c29
Matthias Zeller <m.zeller1901@gmail.com>
2023-08-30 18:55:36 -0400

    itpn 3d

---
+++ configs/_base_/datasets/ivoct-polar-gray_3D-3-slices_bs128_mae.py
@@ -0,0 +1,36 @@
+# dataset settings
+dataset_type = 'IVOCTDataset'
+data_root = 'data/shockwave/images-3D-polar'
+
+data_preprocessor = dict(
+    type='SelfSupDataPreprocessor',
+    # mean=[123.675, 116.28, 103.53],
+    mean=[24.87],
+    # std=[58.395, 57.12, 57.375],
+    std=[53.36],
+    #to_rgb=True
+)
+
+train_pipeline = [
+    dict(type='LoadImageFromZipFile', imdecode_backend='pillow', color_type='unchanged', stack_neighbour_slices=1),
+    dict(type='RandomFlip', prob=0.5, direction='vertical'),
+    dict(type='RandomRoll', axis=0),
+    dict(type='PackInputs')
+]
+
+val_pipeline = [
+    dict(type='LoadImageFromZipFile', imdecode_backend='pillow', color_type='unchanged', stack_neighbour_slices=1),
+    dict(type='PackInputs')
+]
+
+train_dataloader = dict(
+    batch_size=128,
+    num_workers=32,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        #split='splits/train.txt',
+        pipeline=train_pipeline))
